eb1e14309b5744be98707c6385b21bae31a539c9
ka1antarBAB <EhsanMalekBS@gmail.com>
2023-11-30 11:03:05 +0330

    make new class in admin.py by name : CustomUserAdmin and exted as UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    pass
