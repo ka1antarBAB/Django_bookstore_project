c863a2397af4da109ad985e91a09214515120661
ka1antarBAB <EhsanMalekBS@gmail.com>
2023-12-01 13:54:06 +0330

    add redirect url for login and logout to Home

--- config/settings.py
+++ config/settings.py
@@ -128,4 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'Home'
+LOGOUT_REDIRECT_URL = 'Home'
+
 
