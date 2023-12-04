41d44d4ab707930a5903549821f6579b511dc600
ka1antarBAB <EhsanMalekBS@gmail.com>
2023-12-04 15:32:51 +0330

    delete the home url from config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
-    path("home/", TemplateView.as_view(template_name="home.html"), name="home"),
 
 ]
