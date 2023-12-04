7f7f715e3e71ded024616c25b1361e11b2d60e96
ka1antarBAB <EhsanMalekBS@gmail.com>
2023-12-04 15:34:55 +0330

    make empty url in config/urls.py and conntect to the pages/urls.py

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
+    path("", include("pages.urls")),
 
 ]
