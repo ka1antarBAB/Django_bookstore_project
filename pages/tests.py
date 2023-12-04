04b4126dc3b1dae5bfaee6e9724e508d1a8e5619
ka1antarBAB <EhsanMalekBS@gmail.com>
2023-12-04 16:11:34 +0330

    write test for pages app (home url by name nad url , tempalte test , contents test)

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_by_url(self):
+        response = self.client.get("")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contents(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, "home page")
+
+    def test_home_page_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, "home.html")
