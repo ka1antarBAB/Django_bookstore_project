c786db7a775d7c3d2067585b2035e10c3065141a
ka1antarBAB <EhsanMalekBS@gmail.com>
2023-12-09 16:51:05 +0330

    new url by name books

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
+    path("books/", include("books.urls")),
 
 ]
